354d4fb601055615d6e16ff9a11f5e5a2567ee4c
thisisrick25 <swapnaneel06@gmail.com>
2025-12-03 19:36:23 +0530

    delete outdated files

--- split_dataset.py
+++
@@ -1,6 +0,0 @@
-import splitfolders
-
-# Split with a ratio of 70/30
-splitfolders.ratio("datasets/Task02_Heart/nifti_files/",
-                   output="datasets/Task02_Heart/data_train_test",
-                   seed=1337, move=False, ratio=(.7, .3))
